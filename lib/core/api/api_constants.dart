83e860e40a7723ac74998cc9881b3258c77e1f3f
Adebisi Covenant <adebisicovenant01@gmail.com>
2026-09-05 19:54:42 +0100

    Configure white-label brand: DakeVentures (com.harkonedesigns.dakeventures)

--- lib/core/api/api_constants.dart
+++ lib/core/api/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
   static const String baseUrl = String.fromEnvironment(
     'BASE_URL',
-    defaultValue: 'https://tincitybill.com/api/v1',
+    defaultValue: 'https://dakeventures.com.ng/api/v1',
   );
 }
