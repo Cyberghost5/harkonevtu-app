46c0d55dacb0714c56a68cd6aa10f405e0e0213c
Adebisi Covenant <adebisicovenant01@gmail.com>
2026-09-08 12:28:16 +0100

    chore: Remove unused import in otp_verification_screen.dart

--- lib/views/auth/otp_verification_screen.dart
+++ lib/views/auth/otp_verification_screen.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 import '../../providers/auth_provider.dart';
+
 import '../../providers/dashboard_provider.dart';
 import '../navigation/main_navigation_shell.dart';
 
